d034d82c1c160b8c67eaf268af0090c2303f763c
thereseh <tah5279@g.rit.edu>
2016-10-02 19:37:23 -0400

    Added a canvas as a temp start menu with buttons which determines num of players. Clicking button will also turn the menu canvas off, and the game canvas on.

---
+++ fireSpreadProto/Assets/Scripts/MenuButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuButtonManager : MonoBehaviour {
+	public Canvas menu;
+	public Canvas game;
+	public int numOfPlayers = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NumOfPlayers(int num)
+	{
+		numOfPlayers = num;
+		menu.enabled = false;
+		game.enabled = true;
+
+	}
+}
