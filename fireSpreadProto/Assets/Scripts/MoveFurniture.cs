6f2ac132a2ba0b0a49365b0e43a9eb1ae0764144
thereseh <tah5279@g.rit.edu>
2016-10-03 16:43:55 -0400

    Furniture is now placed on board, you can move them around with arrow keys and R for rotate. Ray cast is used to check which furniture that's selected

---
+++ fireSpreadProto/Assets/Scripts/MoveFurniture.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFurniture : MonoBehaviour {
+	public bool selected = false;
+	private Vector3 pos;
+	private bool horizontal = true;
+
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 screenPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		//RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			if(Physics.Raycast(ray, out hit) && hit.transform.name == gameObject.transform.name) {
+				print("clicked on");
+				selected = true;
+			}else{
+				print("clicked outside");
+				selected = false;
+			}
+			
+		}
+
+		Move ();
+	}
+
+	void Move()
+	{
+		if (selected) 
+		{
+			if (Input.GetKeyDown (KeyCode.LeftArrow))
+			{
+				pos.x -= 1;
+				gameObject.transform.position = pos;
+			}
+			if (Input.GetKeyDown (KeyCode.RightArrow))
+			{
+				pos.x += 1;
+				gameObject.transform.position = pos;
+			}
+			if (Input.GetKeyDown (KeyCode.UpArrow))
+			{
+				pos.z += 1;
+				gameObject.transform.position = pos;
+			}
+			if (Input.GetKeyDown (KeyCode.DownArrow))
+			{
+				pos.z -= 1;
+				gameObject.transform.position = pos;
+			}
+
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				gameObject.transform.Rotate(Vector3.up, 90);
+				if (horizontal)
+				{
+					pos.x += 0.5f;
+					pos.z += 0.5f;
+					horizontal = !horizontal;
+					gameObject.transform.position = pos;
+				}
+				else {
+					pos.x -= 0.5f;
+					pos.z -= 0.5f;
+					horizontal = !horizontal;
+					gameObject.transform.position = pos;
+				}
+				print ("Rotate called");
+			}
+		}
+	}
+}
--- fireSpreadProto/Assets/Scripts/SelectFurniture.cs
+++ fireSpreadProto/Assets/Scripts/SelectFurniture.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class SelectFurniture : MonoBehaviour
 {
     public static string currentSelection = "";
+	public GameObject[] furniture;
+	private Vector3 pos;
 
 	void Start ()
     {
@@ -16,6 +18,29 @@ public class SelectFurniture : MonoBehaviour
         {
             print("Selected " + name);
             currentSelection = name;
+
+			foreach (GameObject furn in furniture)
+			{
+				if (furn.name == name)
+				{
+					if (furn.name == "Couch")
+					{
+						pos = new Vector3(6.5f,0.625f,6f);
+					}
+					else {
+						pos = new Vector3(6.0f,0.625f,6f);
+					}
+					GameObject copy = (GameObject)(Instantiate(furn, pos, Quaternion.identity));
+					MoveFurniture copyCode = copy.GetComponent<MoveFurniture>();
+					copyCode.selected = true;
+				}
+				else
+				{
+					GameObject copy = furn;
+					MoveFurniture copyCode = copy.GetComponent<MoveFurniture>();
+					copyCode.selected = false;
+				}
+			}
         }
         else
         {
