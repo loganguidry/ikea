faaf8d83cef2528969b623ea49ce314c92717d0d
thereseh <tah5279@g.rit.edu>
2016-10-04 09:39:54 -0400

    Tried adding  OnParticleCollision(GameObject other) to detect if furniture is colliding with fire. Is almost working, need to do some tweaking. The firePreFab is not assigned (after changing the fire prefab) and I tried to assign it but it won't work, needs to be fixed.

---
+++ fireSpreadProto/Assets/Scripts/CheckOnFire.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckOnFire : MonoBehaviour {
+	public bool onFire = false;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnParticleCollision(GameObject other) {
+		print ("hit by fire");
+	}
+}
